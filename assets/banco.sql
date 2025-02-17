65414af438f356593f1d56490de8ca756fc7b934
Thaaleslimarj <thaaleslimarj@gmail.com>
2025-02-17 14:30:53 -0300

    editando o bd

--- assets/banco.sql
+++ assets/banco.sql
@@ -1,16 +1,17 @@
-CREATE TABLE usuario (
-id INT NOT NULL AUTO_INCREMENT,
-usuario VARCHAR(45) NOT NULL,
-rg INT NOT NULL,
-data_nascimento DATE NOT NULL,
-senha VARCHAR(200) NOT NULL,
-PRIMARY KEY (`id`));
+CREATE DATABASE sislogin;
+use sislogin;
 
 CREATE TABLE funcionario (
 id int auto_increment PRIMARY KEY,
 nome varchar(255),
 login varchar(255),
 senha varchar(255),
-tipo enum('admin','usuario'),
+tipo_funcionario int,
+FOREIGN KEY (tipo_funcionario) REFERENCES tipo_funcionario(id),
 status enum('ativo','inativo')
-)
+);
+ 
+CREATE TABLE tipo_funcionario (
+id int auto_increment PRIMARY KEY,
+tipo varchar (255)
+);
